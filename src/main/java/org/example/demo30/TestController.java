a01f4a052d81c5b9268cbe607238c6e25959246b
sangwoo <lesw1216@gmail.com>
2025-09-10 16:21:20 +0900

    test04

--- src/main/java/org/example/demo30/TestController.java
+++ src/main/java/org/example/demo30/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity test() {
-        return ResponseEntity.ok("test teststest");
+        return ResponseEntity.ok("test04");
     }
 }
